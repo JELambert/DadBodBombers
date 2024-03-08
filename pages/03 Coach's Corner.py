0d8010244e32f961426ec1f71a8475aaf553a41c
Josh <joshua.lambert@knights.ucf.edu>
2024-03-08 12:07:39 -0600

    add ryan cc

--- pages/03 Coach's Corner.py
+++ pages/03 Coach's Corner.py
@@ -61,7 +61,7 @@ def coaching():
                     
         users = ['Beep', 'Tyler', 'LambertDBB', 'Grace', 'Ben', 'Forrest','Spangler',
                  'Sweet', 'Cody',  'Niko',  'Dan', 'Renzo', 'Sean', 'Shack', 'Frank',
-                 'Blake', 'Todd', 'Taylor', 'Connor']
+                 'Blake', 'Todd', 'Taylor', 'Connor', 'Ryan']
 
         emails = [st.secrets[x]['email'] for x in users]
         fileIds = [st.secrets[x]['fileid'] for x in users]
